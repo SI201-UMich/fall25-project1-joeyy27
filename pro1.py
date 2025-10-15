6fc2a1b5822b85a0e31ef005ef6a50160fcddd66
Joey Yang <joeyy@umich.edu>
2025-10-15 15:25:30 -0400

    fixed using None to 0 because it failed a case

--- pro1.py
+++ pro1.py
@@ -33,10 +33,10 @@ def island_species_average(data, species_col="species", body_col="body_mass_g", 
 
     result = {}
     for sp, sex_dict in species_dict.items():
-        male_avg = round(sum(sex_dict["male"]) / len(sex_dict["male"])) if sex_dict["male"] else None
-        female_avg = round(sum(sex_dict["female"]) / len(sex_dict["female"])) if sex_dict["female"] else None
+        male_avg = round(sum(sex_dict["male"]) / len(sex_dict["male"])) if sex_dict["male"] else 0
+        female_avg = round(sum(sex_dict["female"]) / len(sex_dict["female"])) if sex_dict["female"] else 0
         total_list = sex_dict["male"] + sex_dict["female"]
-        total_avg = round(sum(total_list) / len(total_list)) if total_list else None
+        total_avg = round(sum(total_list) / len(total_list)) if total_list else 0 #changed None to 0 
         result[sp] = {"average": total_avg, "male": male_avg, "female": female_avg}
     return result
 
