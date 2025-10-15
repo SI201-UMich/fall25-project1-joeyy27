2ae6f831ee31ee32dae9062c5c324908b56e9dd6
Joey Yang <joeyy@umich.edu>
2025-10-15 14:55:02 -0400

    Add CSV loading function

--- pro1.py
+++ pro1.py
@@ -1,1 +1,14 @@
-#hi
+import csv
+
+def load_csv(file_path):
+    """Load CSV into list of dictionaries."""
+    data = []
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
+
+def island_species_average(data, species_col="species", body_col="body_mass_g", sex_col="sex"):
+    """Average body mass per species and by sex."""
+   
