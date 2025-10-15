a45e7cab272dc34f73c263fe381c83fb005ba212
Joey Yang <joeyy@umich.edu>
2025-10-15 15:02:30 -0400

    Implement flipper_length_trend function, Add test cases for both functions. fixed edge cases for flipper seems like there is still an error

--- pro1.py
+++ pro1.py
@@ -31,3 +31,33 @@ def island_species_average(data, species_col="species", body_col="body_mass_g", 
         total_avg = round(sum(total_list) / len(total_list)) if total_list else 0
         result[sp] = {"average": total_avg, "male": male_avg, "female": female_avg}
     return result
+
+def flipper_length_trend(data, flipper_col="flipper_length_mm", year_col="year", species_col="species"):
+    """Average flipper length per species per year."""
+    trend_dict = {}
+    species_set = set()
+
+    for row in data:
+        sp = row[species_col]
+        species_set.add(sp)
+        year = row[year_col]
+        try:
+            flipper = float(row[flipper_col])
+        except:
+            continue  
+
+        if sp not in trend_dict:
+            trend_dict[sp] = {}
+        if year not in trend_dict[sp]:
+            trend_dict[sp][year] = []
+        trend_dict[sp][year].append(flipper)
+
+    result = {}
+    for sp in species_set:  
+        result[sp] = {}
+        if sp in trend_dict:
+            for yr, lengths in trend_dict[sp].items():
+                if lengths:
+                    result[sp][int(yr)] = round(sum(lengths) / len(lengths), 1)
+    return result
+
